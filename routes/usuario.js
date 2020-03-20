e42cc6e316270f95c267c27f615ef2605109002c
AndresNG97 <AndresNG1997@gmail.com>
2020-03-20 02:13:59 +0100

    Test Heroku

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,77 +1,13 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const _ = require("underscore");
-const jwt = require("jsonwebtoken");
-const Usuario = require("../models/usuario");
-const app = express();
+const UserController = require("../controllers/usuario");
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const api = express.Router();
+api.use(express.urlencoded({ extended: false }));
 
-app.post("/register", (req, res) => {
-  let body = req.body;
+// parse application/json
+api.use(express.json());
 
-  let usuario = new Usuario({
-    name: body.name,
-    lastname: body.lastname,
-    email: body.email,
-    password: bcrypt.hashSync(body.password, 10)
-  });
+api.post("/register", UserController.register);
+api.post("/login", UserController.login);
 
-  usuario.save((err, usuarioDB) => {
-    if (err) {
-      return res.status(400).json({
-        ok: false,
-        err
-      });
-    }
-
-    res.json({
-      ok: true,
-      usuario: usuarioDB
-    });
-  });
-});
-
-app.post("/login", (req, res) => {
-  let body = req.body;
-
-  Usuario.findOne(
-    { email: { $regex: new RegExp("^" + body.email, "i") } },
-    (err, usuarioDB) => {
-      if (err) {
-        return res.status(500).json({
-          ok: false,
-          err
-        });
-      }
-      if (!usuarioDB) {
-        return res.status(400).json({
-          ok: false,
-          err: {
-            message: "(Email) o Contraseña incorrectos"
-          }
-        });
-      }
-      if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
-        return res.status(400).json({
-          ok: false,
-          err: {
-            message: "Email o (Contraseña) incorrectos"
-          }
-        });
-      }
-      let Authorization = jwt.sign({ usuarioDB }, process.env.SEED_TOKEN, {
-        expiresIn: process.env.CADUCIDAD_TOKEN
-      });
-
-      res.json({
-        ok: true,
-        usuarioDB,
-        Authorization
-      });
-    }
-  );
-});
-
-module.exports = app;
+module.exports = api;
