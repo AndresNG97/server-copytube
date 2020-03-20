7c56dd70f553442fc6a3416f2e9cb4f80d8410d4
AndresNG97 <AndresNG1997@gmail.com>
2020-03-20 01:09:02 +0100

    Heroku Testing

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -32,42 +32,39 @@ function register(req, res) {
 function login(req, res) {
   let body = req.body;
 
-  Usuario.findOne(
-    { email: { $regex: new RegExp("^" + body.email, "i") } },
-    (err, usuarioDB) => {
-      if (err) {
-        return res.status(500).json({
-          ok: false,
-          err
-        });
-      }
-      if (!usuarioDB) {
-        return res.status(400).json({
-          ok: false,
-          err: {
-            message: "(Email) o Contraseña incorrectos"
-          }
-        });
-      }
-      if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
-        return res.status(400).json({
-          ok: false,
-          err: {
-            message: "Email o (Contraseña) incorrectos"
-          }
-        });
-      }
-      let Authorization = jwt.sign({ usuarioDB }, process.env.SEED_TOKEN, {
-        expiresIn: process.env.CADUCIDAD_TOKEN
-      });
-
-      res.json({
-        ok: true,
-        usuarioDB,
-        Authorization
+  Usuario.findOne({ email: body.email.toLowerCase() }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err
       });
     }
-  );
+    if (!usuarioDB) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: "(Email) o Contraseña incorrectos"
+        }
+      });
+    }
+    if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: "Email o (Contraseña) incorrectos"
+        }
+      });
+    }
+    let Authorization = jwt.sign({ usuarioDB }, process.env.SEED_TOKEN, {
+      expiresIn: process.env.CADUCIDAD_TOKEN
+    });
+
+    res.json({
+      ok: true,
+      usuarioDB,
+      Authorization
+    });
+  });
 }
 
 module.exports = {
